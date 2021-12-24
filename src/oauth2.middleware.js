84d9fbaa3e0b5d19e83942f0a2641355e25a9d3e
Luis Huertas <hu3rtas@gmail.com>
2021-12-24 13:55:02 -0500

    wrappertest

--- src/oauth2.middleware.js
+++ src/oauth2.middleware.js
@@ -1,14 +1,38 @@
 const oauthBoot = () => {
   const boot = {};
 
-  boot.addEndPoints = (expressApp) => {
+  boot.bootExpress = (expressApp) => {
+    expressApp.post = (path, allowed, ...handler) => {
+      expressApp.set(path, allowed);
+      return expressApp.post(path, ...handler);
+    };
+
+    expressApp.get = (path, allowed, ...handler) => {
+      expressApp.set(path, allowed);
+      return expressApp.post(path, ...handler);
+    };
+
+    expressApp.put = (path, allowed, ...handler) => {
+      expressApp.set(path, allowed);
+      return expressApp.post(path, ...handler);
+    };
+
+    expressApp.delete = (path, allowed, ...handler) => {
+      expressApp.set(path, allowed);
+      return expressApp.post(path, ...handler);
+    };
+
+    return expressApp;
+  };
+
+  boot.addEndPoints = (expressBootApp) => {
     expressApp.get("/auth", (req, res) => {
       res.json({ x: false });
     });
   };
 
   boot.guard = (req, res, next) => {
-    console.log("hi");
+    console.log(req.path);
     next();
   };
 
