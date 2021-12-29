a2ce6f70c3dc168261c2f604f591db7469fa5c55
Luis Huertas <hu3rtas@gmail.com>
2021-12-29 15:52:42 -0500

    ...

--- src/oauth2.middleware.js
+++ src/oauth2.middleware.js
@@ -594,6 +594,7 @@ class OauthBoot {
       const exp = this.expressSecured.get(req.path);
       console.log("exp", exp);
       if (exp === ":") {
+        console.log("here");
         next();
       }
       return res.json({ code: 403100, message: "User not authorized" });
