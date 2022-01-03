e2b7ffa237f97afd46585cf6c9f8809ac066490d
Luis Huertas <hu3rtas@gmail.com>
2022-01-03 12:27:45 -0500

    ...

--- src/oauth2.middleware.js
+++ src/oauth2.middleware.js
@@ -618,42 +618,40 @@ class OauthBoot {
       return expressApp.get(path, ...handler);
     };
 
-    // expressApp.put = (path, allowed, ...handler) => {
-    //   expressApp.set(path, allowed);
-    //   return expressApp.post(path, ...handler);
-    // };
+    expressApp.put = (path, allowed, ...handler) => {
+      expressApp.set(path, allowed);
+      return expressApp.put(path, ...handler);
+    };
 
     expressApp.obDelete = (path, allowed, ...handler) => {
       expressApp.set(path, allowed);
       return expressApp.delete(path, ...handler);
     };
 
-    // console.log("xx");
     return expressApp;
   }
 
   static bootOauthExpressRouter(expressRouter) {
-    // expressApp.post = (path, allowed, ...handler) => {
-    //   expressApp.set(path, allowed);
-    //   return expressApp.post(path, ...handler);
-    // };
+    expressRouter.obPost = (path, allowed, ...handler) => {
+      expressRouter.set(path, allowed);
+      return expressRouter.post(path, ...handler);
+    };
 
     expressRouter.obGet = (path, allowed, ...handler) => {
       expressRouter.set(path, allowed);
       return expressRouter.get(path, ...handler);
     };
 
-    // expressApp.put = (path, allowed, ...handler) => {
-    //   expressApp.set(path, allowed);
-    //   return expressApp.post(path, ...handler);
-    // };
+    expressRouter.put = (path, allowed, ...handler) => {
+      expressRouter.set(path, allowed);
+      return expressRouter.put(path, ...handler);
+    };
 
     expressRouter.obDelete = (path, allowed, ...handler) => {
       expressRouter.set(path, allowed);
       return expressRouter.delete(path, ...handler);
     };
 
-    // console.log("xx");
     return expressRouter;
   }
 
